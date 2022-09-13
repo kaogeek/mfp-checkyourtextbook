9905d63b146ec67a7853047d8859bfa8ec2f02f6
nu-ch <nuchaiwongthon@gmail.com>
2022-09-13 22:20:50 +0700

    Implement infinities scroll

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
   // import type { User } from '$models';
+  import InfiniteLoading from 'svelte-infinite-loading';
+
   import {
     Swiper,
     GridContent,
@@ -12,6 +14,64 @@
   // export let data: { payload: User[] };
   // export let errors: any;
 
+  let imageRd = [
+    'https://i.ytimg.com/vi/aILNohZZJSs/maxresdefault.jpg',
+    'https://pbs.twimg.com/media/ElABjdUWAAEEhA-.jpg',
+    'https://storage-wp.thaipost.net/2021/10/%E0%B8%9B%E0%B8%A3%E0%B8%B0%E0%B8%A7%E0%B8%B4%E0%B8%95%E0%B8%A3-%E0%B8%A7%E0%B8%87%E0%B8%A9%E0%B9%8C%E0%B8%AA%E0%B8%B8%E0%B8%A7%E0%B8%A3%E0%B8%A3%E0%B8%93-%E0%B8%9E%E0%B8%A5.%E0%B8%AD-170264.jpg',
+    'https://img.pptvhd36.com/thumbor/2020/07/31/news-3d2daa9f7e.jpg',
+  ];
+
+  // $: items = [
+  //   ...Array.from({ length: 20 }, (_, index) => {
+  //     return {
+  //       id: index,
+  //       img: imageRd[
+  //         Math.round(Math.floor(Math.random() * (imageRd.length - 0 + 0) + 0))
+  //       ],
+  //       title: 'ทำไมไม่มีเรื่องรัฐประหารในหนังสือประวัติศาสตร์ไทย',
+  //     };
+  //   }),
+  // ];
+
+  const api = 'https://hn.algolia.com/api/v1/search_by_date?tags=story';
+
+  let page = 1;
+  let items: any[] = [];
+
+  function infiniteHandler({
+    detail: { loaded, complete },
+  }: {
+    detail: {
+      loaded: any;
+      complete: any;
+    };
+  }) {
+    fetch(`${api}&page=${page}`)
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.hits.length) {
+          page += 1;
+          items = [
+            ...items,
+            ...data.hits.map((e: any, index: number) => {
+              return {
+                id: items.length + index,
+                img: imageRd[
+                  Math.round(
+                    Math.floor(Math.random() * (imageRd.length - 0 + 0) + 0)
+                  )
+                ],
+                title: 'ทำไมไม่มีเรื่องรัฐประหารในหนังสือประวัติศาสตร์ไทย',
+              };
+            }),
+          ];
+
+          loaded();
+        } else {
+          complete();
+        }
+      });
+  }
 </script>
 
 <svelte:head>
@@ -41,9 +101,10 @@
     weight="medium"
     opacity={1}>ช่วยกันตรวจสอบความบ้งของการศึกษาไทย</P
   >
-  <GridContent />
-</section>
+  <GridContent bind:items />
 
+  <InfiniteLoading on:infinite={infiniteHandler} />
+</section>
 
 <style>
 </style>
